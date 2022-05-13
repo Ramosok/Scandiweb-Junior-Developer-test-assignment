7a98cd73381e8327a9e421580a5f8e66d452967f
Ramosok <Ramosok86@gmail.com>
2022-05-13 06:04:00 +0300

    trenning GraphQl Apollo

--- src/graphQL/index.js
+++ src/graphQL/index.js
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+const BASE_URL = "http://localhost:4000/graphql";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: BASE_URL,
   cache: new InMemoryCache(),
 });
