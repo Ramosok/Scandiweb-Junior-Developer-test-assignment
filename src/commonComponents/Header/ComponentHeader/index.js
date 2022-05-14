d6c57f56b98da368ddb2bc3f0e2ac8ee4460912c
vmaslionik <vitali.maslionik@digitalmara.com>
2022-05-14 16:16:58 +0300

    add select currencies

--- src/commonComponents/Header/ComponentHeader/index.js
+++ src/commonComponents/Header/ComponentHeader/index.js
@@ -1,19 +1,27 @@
 import { memo } from "react";
 
-import PropTypes, { arrayOf } from "prop-types";
+import PropTypes, { arrayOf, objectOf } from "prop-types";
 
 import { Header } from "../HeaderStyledComponents";
 import { LinkItems } from "./LinkItems";
 
-export const ComponentHeader = memo(({ categories }) => {
+export const ComponentHeader = memo(({ categories, currencies }) => {
   return (
     <Header>
       {categories?.map(({ name }) => (
         <LinkItems key={name} name={name} />
       ))}
+      <select name="1" id="1">
+        {currencies.currencies?.map(({ label, symbol }) => (
+          <option key={label} value={label}>
+            {symbol} {label}
+          </option>
+        ))}
+      </select>
     </Header>
   );
 });
 ComponentHeader.propTypes = {
   categories: arrayOf(PropTypes.object).isRequired,
+  currencies: objectOf(PropTypes.array).isRequired,
 };
